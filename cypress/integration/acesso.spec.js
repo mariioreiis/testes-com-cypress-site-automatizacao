b546fc1e3115130f171adf84158f20f35c7c2d7d
mario <marioreis1236@gmail.com>
2022-05-25 18:42:18 -0300

    Implementação do caso de teste para validar o formulário Contact Us

--- cypress/integration/acesso.spec.js
+++ cypress/integration/acesso.spec.js
@@ -39,17 +39,54 @@ describe('Testes no site My Store', function() {
         .should('be.visible')
       })
 
-      it.only('Preencher os campos do "Contact Us" e enviar', function(){
+      it('Preencher os campos do "Contact Us" e enviar, voltar a Home no final da execução', function(){
         cy.get('#contact-link > a')
         .should('be.visible')
         .click()
 
         cy.get('#id_contact')
-        .select(2)
-        .should('have.value', 'Customer service')
+        .select('Webmaster')
+        .should('have.value', 1)
+
+        cy.get('#email')
+        .should('be.visible')
+        .clear()
+        .type('mario@gmail.com')
+        .should('not.have.text')
+
+        cy.get('#id_order')
+        .should('be.visible')
+        .clear()
+        .type('lalalala')
+        .should('not.have.text')
+
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/pj3.jpg')
+        .should(function($input) {
+           expect($input[0].files[0].name).to.equal('pj3.jpg')
+        })
   
-        //cy.get('#submitMessage > span')
-        //.should('be.visible')
-        //.click()
+        cy.get('#message')
+        .should('be.visible')
+        .clear()
+        .type('este campo está receendo um teste automatizado utilizando o cypress ...')
+        .should('not.have.text')
+
+        cy.get('#submitMessage > span')
+        .should('be.visible')
+        .click()
+
+        cy.get('.alert')
+        .should('be.visible')
+
+        cy.get('li > .btn > span')
+        .should('be.visible')
+        .click()
+        cy.title()
+        .should('eq', 'My Store')
+        cy.get('.logo')
+        .should('be.visible')
+        
       })
     })
